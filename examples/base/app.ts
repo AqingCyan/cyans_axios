022c2a65132a43715f4e4691aae6c2ae07e89890
AqingCyan <aqingcyan@cyanthing.com>
2019-06-23 02:55:19 +0800

    feat(examples): 构建post请求测试axios对data数据的转换是否能实现传输

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -60,3 +60,20 @@ axios({
     bar: 'baz'
   }
 })
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+const arr = new Int32Array([21, 23])
+
+axios({
+  method: 'post',
+  url: '/base/buffer',
+  data: arr
+})
