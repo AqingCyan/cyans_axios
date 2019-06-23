1e1ac716ad23ef0cc92ee6f122389af6600c5985
AqingCyan <aqingcyan@cyanthing.com>
2019-06-23 17:44:47 +0800

    feat(src): 规范响应体中的header为键值对格式
    将响应体中的header处理，从字符串拆分成键值对形式响应

--- src/helpers/headers.ts
+++ src/helpers/headers.ts
@@ -35,3 +35,26 @@ export function processHeaders(headers: any, data: any): any {
   }
   return headers
 }
+
+/**
+ * 将headers转化为对象
+ * @param headers
+ */
+export function parseHeaders(headers: string): any {
+  let parsed = Object.create(null)
+  if (!headers) {
+    return parsed
+  }
+  headers.split('\r\n').forEach(line => {
+    let [key, val] = line.split(':')
+    key = key.trim().toLowerCase()
+    if (!key) {
+      return
+    }
+    if (val) {
+      val = val.trim()
+    }
+    parsed[key] = val
+  })
+  return parsed
+}
--- src/xhr.ts
+++ src/xhr.ts
@@ -2,6 +2,7 @@
  * 发送请求文件
  */
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
+import { parseHeaders } from './helpers/headers'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise(resolve => {
@@ -19,7 +20,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       if (request.readyState !== 4) {
         return
       }
-      const responseHeaders = request.getAllResponseHeaders()
+      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
       const responseData = responseType !== 'text' ? request.response : request.responseText
       const response: AxiosResponse = {
         data: responseData,
