bbe8a71bf2c269c36e6f347cb1f4e78bc05f253c
AqingCyan <aqingcyan@cyanthing.com>
2019-06-23 14:12:25 +0800

    feat(examples): 构建测试headers的例子

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -64,10 +64,22 @@ axios({
 axios({
   method: 'post',
   url: '/base/post',
+  headers: {
+    'content-type': 'application/json;charset=utf-8'
+  },
   data: {
     a: 1,
     b: 2
   }
+})
+
+const paramsString = 'q=URLUtils.searchParams&topic=api'
+const searchParams = new URLSearchParams(paramsString)
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: searchParams
 })
 
 const arr = new Int32Array([21, 23])
