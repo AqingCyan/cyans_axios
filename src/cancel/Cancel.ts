6acfd2a5d1eab5e9278e43df524c8d86f68aadd9
AqingCyan <xuekaiqi@creatshare.com>
2019-07-11 21:49:09 +0800

    feat(cancel): 增加取消操作接口与类

---
+++ src/cancel/Cancel.ts
@@ -0,0 +1,14 @@
+/*
+ * Cancel类
+ */
+export default class Cancel {
+  message?: string
+  constructor(message?: string) {
+    this.message = message
+  }
+}
+
+// 判断是否cancel实例
+export function isCancel(value: any): boolean {
+  return value instanceof Cancel
+}
