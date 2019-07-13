8e95ee3f44be19cfdef073cef845e1b2a749c93a
AqingCyan <xuekaiqi@creatshare.com>
2019-07-13 18:35:10 +0800

    test(cookie and data): 完成cookie与data模块测试100%

---
+++ test/helpers/cookie.spec.ts
@@ -0,0 +1,17 @@
+/*
+ * cookie模块测试
+ */
+
+import cookie from '../../src/helpers/cookie'
+
+describe('helpers:cookie', () => {
+  test('should read cookies', () => {
+    document.cookie = 'foo=baz'
+    expect(cookie.read('foo')).toBe('baz')
+  })
+
+  test('should return null if cookie name is not exist', () => {
+    document.cookie = 'foo=baz'
+    expect(cookie.read('bar')).toBeNull()
+  })
+})
